3e5856fbef0bd34fbda484311ba5e427f6f230a5
Macmizy <macmizy@gmail.com>
2023-09-25 15:24:16 +0100

    commit

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
 const adminRouter = require('./routes/admin.routes/auth.route')
 const workerRouter = require('./routes/admin.routes/worker.route')
 const categoryRouter = require('./routes/admin.routes/category.route')
@@ -7,17 +9,39 @@ const transportationRouter = require('./routes/admin.routes/transportation.route
 const feedbackRouter = require('./routes/admin.routes/feedback.route')
 const customerRouter = require("./routes/admin.routes/customer.route")
 
-const userRouter = require('./routes/user.routes/auth.route')
+const userRoute = require('./routes/user.routes/auth.route')
 const feedbackRoute = require('./routes/user.routes/feedback.route')
+const workerRoute = require('./routes/user.routes/worker.route')    
+const categoryRoute = require('./routes/user.routes/category.route')
+const transportationRoute = require('./routes/user.routes/transportation.route')
 
 require('./db.js').connectToMongoDB()
 require("./authentication/auth.js")
 require("dotenv").config()
+const logger = require('./utils/logger/logger')
 
 const app = express()
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use(limiter)
+app.use(helmet());
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Home Page")
+    console.log("Welcome to Home Page")
+
+})
+
 
 app.use('/api/admin', adminRouter)
 app.use('/api/admin/worker', workerRouter)
@@ -28,8 +52,12 @@ app.use('/api/admin/feedback', feedbackRouter)
 app.use('/api/admin/customer', customerRouter)
 
 
-app.use('/api/user', userRouter)
+app.use('/api/user', userRoute)
 app.use('/api/user/feedback', feedbackRoute)
+app.use('/api/user/worker', workerRoute)
+app.use('/api/user/category', categoryRoute)
+app.use('/api/user/transportation', transportationRoute)
+
 
 
 
